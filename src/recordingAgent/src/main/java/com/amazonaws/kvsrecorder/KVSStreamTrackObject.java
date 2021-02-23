dd1c695b70007d2a3b6cf2001a59919d8b08b631
Akash Shrestha <shreakas@amazon.com>
2021-02-23 08:55:14 -0800

    Single pass through KVS stream to get both FROM and TO fragments

--- src/recordingAgent/src/main/java/com/amazonaws/kvsrecorder/KVSStreamTrackObject.java
+++ src/recordingAgent/src/main/java/com/amazonaws/kvsrecorder/KVSStreamTrackObject.java
@@ -8,40 +8,14 @@ import java.io.InputStream;
 import java.nio.file.Path;
 
 public class KVSStreamTrackObject {
-    private InputStream inputStream;
-    private StreamingMkvReader streamingMkvReader;
-    private FragmentMetadataVisitor.BasicMkvTagProcessor tagProcessor;
-    private FragmentMetadataVisitor fragmentVisitor;
     private Path saveAudioFilePath;
     private FileOutputStream outputStream;
     private String trackName;
 
-    public KVSStreamTrackObject(InputStream inputStream, StreamingMkvReader streamingMkvReader,
-                                FragmentMetadataVisitor.BasicMkvTagProcessor tagProcessor, FragmentMetadataVisitor fragmentVisitor,
-                                Path saveAudioFilePath, FileOutputStream outputStream, String trackName) {
-        this.inputStream = inputStream;
-        this.streamingMkvReader = streamingMkvReader;
-        this.tagProcessor = tagProcessor;
-        this.fragmentVisitor = fragmentVisitor;
+    public KVSStreamTrackObject(Path saveAudioFilePath, FileOutputStream outputStream, String trackName) {
         this.saveAudioFilePath = saveAudioFilePath;
         this.outputStream = outputStream;
         this.trackName = trackName;
-    }
-
-    public InputStream getInputStream() {
-        return inputStream;
-    }
-
-    public StreamingMkvReader getStreamingMkvReader() {
-        return streamingMkvReader;
-    }
-
-    public FragmentMetadataVisitor.BasicMkvTagProcessor getTagProcessor() {
-        return tagProcessor;
-    }
-
-    public FragmentMetadataVisitor getFragmentVisitor() {
-        return fragmentVisitor;
     }
 
     public Path getSaveAudioFilePath() {
